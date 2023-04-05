23620cdb64fdff569deb93d4e9526fc9c7a09259
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-04-05 09:28:38 -0400

    Add files via upload

---
+++ PGY3121-011D-main/pagina JS/js/funciones.js
@@ -0,0 +1,124 @@
+//console.log("Hola mundo");
+//alert("Hola mundo");
+
+//comentario de una linea
+/*  */
+
+let variableUno = "Hola";
+let variableDos = "mundo";
+console.log(variableUno + " " + variableDos);
+//alert(variableUno + " " + variableDos);
+const constante = 10;
+
+
+let numeroUno = 50;
+let numeroDos = 20;
+
+
+let result = numeroUno + numeroDos;
+
+result += 100;
+
+
+
+console.log("La suma de dos numeros es : " + result);
+
+
+let color = "verde";
+
+if (color == "rojo") {
+    console.log("Es de color rojo.");
+}
+else{
+    console.log("No es de color rojo.");
+}
+
+
+
+let var1 = 10;
+let var2 = 20;
+
+if (var1 === var2) {
+    console.log("Son iguales");
+}
+else{
+    console.log("No son iguales");
+}
+
+
+if (var1 > var2 && var1 > 15 ) {
+    console.log("Es mayor  111");
+}
+else{
+    console.log("Es menor 1111");
+}
+
+
+if (var1 > var2 || var1 > 5 ) {
+    console.log("Es mayor  222");
+}
+else{
+    console.log("Es menor 222");
+}
+
+
+if (var1 > var2) {
+    console.log("Es menor var1", var1);
+}
+else if (var1 > 20) {
+    console.log("Es mayor a 20");
+}else{
+    console.log("No se cumplió");
+}
+
+
+function miFuncion(nomUser) {
+    console.log("Mi primera funcion",nomUser);
+}
+
+miFuncion(10);
+
+let condicional = true;
+
+//1 = true
+//0 = false
+
+if (condicional === 1) {
+    console.log("Es verdadera");
+}else{
+    console.log("Es falso");
+}
+
+function eventOnblur(){
+    //console.log("Evento onBlur");
+    ontenerValorInput();
+}
+
+function eventFocus(){
+    console.log("Evento onFocus");
+}
+
+function eventClick(){
+    console.log("Evento click");
+}
+
+function eventKeyDown(){
+    console.log("Evento key down");
+}
+
+function eventKeyUp(){
+    console.log("Evento key up");
+}
+
+function eventDblClick(){
+    console.log("Evento doble click");
+}
+
+function eventChange(){
+    console.log("Evento change");
+}
+
+function ontenerValorInput(){
+    let var1 = document.getElementById("txtNumero1").value;
+    console.log("Valor input: " + var1);
+}
