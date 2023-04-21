cc0e7cf7fe1c8aedada47e364aad5b1859e805e9
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-04-21 09:44:25 -0400

    Add files via upload

--- PGY3121-011D-main/Pagina jQuery/js/jquery.js
+++ PGY3121-011D-main/Pagina jQuery/js/jquery.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
-    alert("Bienvenido");
+    //alert("Bienvenido");
 })
 
 $(function(){
-    
+    $("p").css({"background-color": "blue"});
+    $(".second").css({"background-color": "red"});
+    $("#tercero").css({"background-color": "rgb(0, 255, 100)"});
 })
+$("#botonMostrar").hide();
+$("#botonOcultar").click(function(){
+    $("#primero").hide();
+    if($("#primero").css({"display" : "none"}) == true){
+        $("#segundo").hide();
+    }
+    $("#botonOcultar").hide();
+    $("#botonMostrar").show();
+})
+$("#botonMostrar").click(function(){
+    $("#primero").show();
+    $("#botonMostrar").hide();
+    $("#botonOcultar").show();
+})
+
+$("#segundo").mouseenter(function(){
+    $("#quinto").fadeOut();
+})
+
+$("#primero").mouseenter(function(){
+    $("#quinto").fadeIn();
+})
+/*setInterval(function(){
+    $("#cuarto").animate({width: "200px"});
+    $("#cuarto").animate({fontSize: "30px"});
+    $("#cuarto").animate({marginLeft: "500px"});
+    $("#cuarto").animate({marginTop: "500px"});
+    $("#cuarto").animate({fontSize: "18px"});
+    $("#cuarto").animate({marginLeft: "0px"});
+    $("#cuarto").animate({marginTop: "0px"});
+    console.log("Intervalo");
+},1000) */
