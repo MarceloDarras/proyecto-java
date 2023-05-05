b6a4644a2f97a2a2f2592973dcb242471587303e
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-05-05 09:51:03 -0400

    Add files via upload

--- PGY3121-011D-main/pagina JS/js/storage.js
+++ PGY3121-011D-main/pagina JS/js/storage.js
@@ -1,9 +1,9 @@
 function crear(){
     let array = [
         {
-            nombre: "Juan",
-            edad: 25,
-            rut: "12.123.123-0"
+            nomber: "Pedro",
+            edad: 50,
+            rut: "10.123.123-0"
         },
         {
             nomber: "Pedro",
@@ -25,10 +25,34 @@ function eliminar(){
 function obtener(){
     let storage = localStorage.getItem("storage011d");
     const obj = JSON.parse(storage);
+
+
     if(obj == null){
         alert("no hay información");
     }else{
-        console.log("Storage", obj);
+        console.log(obj);
     }   
 
+}
+
+function eliminarUnoStorage(){
+    let valor = document.getElementById("txtRut").value;
+    console.log(valor.replace(".","").replace(".","").replace("-", ""));
+    let storage = JSON.parse(localStorage.getItem("storage011d"));
+    let arrayTemporal = [];
+
+    let filtro = storage.filter(e => e.rut == valor);
+    if(filtro.length == 0){
+        alert("No se encontró el rut ingresado " + valor);
+    }else{
+    for (const i of storage) {
+        if(i.rut != valor){
+            arrayTemporal.push(i);
+        }
+        
+    }
+    let objJson2 = JSON.stringify(arrayTemporal)
+    localStorage.setItem("storage011d", objJson2);
+    console.log("Array Temporal ", arrayTemporal);
+    }
 }
